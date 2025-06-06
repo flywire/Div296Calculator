f6c4cbc9930b36b116c50d6a1a3e205caa547a55
flywire <flywire@gmail.com>
2025-06-07 00:36:12 +1000

    Simplify test

--- calc.test.js
+++ calc.test.js
@@ -2,7 +2,7 @@ const { div296Calc } = require('./calc');
 
 test('Example 1.1: Keny', () => {
   const r = div296Calc({ tsb_start: 150000, tsb_end: 165000, after_tax_contrib: 0, withdrawals: 0 });
-  expect(r.taxable).toMatch(/No, Total Super Balance ≤ \$3M/);
+  expect(r.taxable).toBe("No, Total Super Balance ≤ \$3M");
 });
 
 test('Example 1.2: Jess', () => {
@@ -35,16 +35,16 @@ test('Example 1.4: MG', () => {
 test('Example 1.5: Lin', () => {
   const r = div296Calc({ tsb_start: 2800000, tsb_end: 3200000, after_tax_contrib: 300000, withdrawals: 0 });
   expect(r.earnings).toBe(-100000);
-  expect(r.taxable).toMatch(/No, Total Super Balance ≤ \$3M/);
+  expect(r.taxable).toBe("No Division 296 taxable earnings");
 });
 
 test('Example 1.6: Jamal', () => {
   const r = div296Calc({ tsb_start: 3200000, tsb_end: 2800000, after_tax_contrib: 0, withdrawals: 0 });
-  expect(r.taxable).toMatch(/No, Total Super Balance ≤ \$3M/);
+  expect(r.taxable).toBe("No, Total Super Balance ≤ \$3M");
 });
 
 test('Example 1.7: Jacob', () => {
   const r = div296Calc({ tsb_start: 9000000, tsb_end: 8000000, after_tax_contrib: 0, withdrawals: 150000 });
   expect(r.earnings).toBe(-850000);
-  expect(r.taxable).toMatch(/No Division 296 taxable earnings/);
+  expect(r.taxable).toBe("No Division 296 taxable earnings");
 });
